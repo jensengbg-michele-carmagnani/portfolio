88f1b9a786a36f483a10f7551767fdbbcf79f328
Michele <michele.carmagnani@gmail.com>
2020-11-21 21:07:53 +0100

    text Bio

--- dist/js/mani.js
+++ dist/js/mani.js
@@ -1,9 +1,23 @@
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("./sw.js")
+    .then((event) => {
+      console.log("Service workder : registerd");
+    })
+    .catch((error) => {
+      console.log("Service worker registration error:", error.event);
+    });
+}
+
 // elements
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
 const navItems = document.querySelectorAll(".nav-item");
+// Copy and paste this code into your main script file
+// The purpose is to register the service worker
+// The browser will load sw.js for us if you do this
 
 // set State of manu
 let showMenu = false;
